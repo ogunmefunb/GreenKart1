cfe8937ae467e961628fd3c8c60ca440cc714276
Autoprime91 <benjermin.ogunmefun@gmail.com>
2021-01-05 12:56:24 +0100

    First Commit

---
+++ src/test/java/GreenKart_Runner/GK_Runner.java
@@ -0,0 +1,17 @@
+package GreenKart_Runner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		strict = true,
+		features = "TestGreenKart1/src/main/resources/GreenKart_Features/GreenKart_checkoutVerification.feature",
+		glue = ""
+		)
+
+
+public class GK_Runner {
+
+}
